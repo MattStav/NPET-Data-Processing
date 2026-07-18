dd17d1d8f28707ae0babfcc3c11ea1f25c279ba9
MattStav <matej.stavinoha@volny.cz>
2026-07-18 18:58:30 +0200

    Added two epochs workflow test

---
+++ tests/tests_workflow/test_two_epochs_workflow.py
@@ -0,0 +1,60 @@
+from unittest.mock import patch
+
+import numpy as np
+from numpy.typing import NDArray
+from NPET_DP.epoch_processing.helper_funcs import import_data, auto_scale_num, get_unit
+from pathlib import Path
+
+from NPET_DP.epoch_processing.helper_processing import (
+    discard_rows_until_first_col_match,
+    calculate_delay,
+    detect_signal,
+    recursive_sigma_filter,
+)
+
+
+def test_two_epochs(data_dir):
+    start_p: Path = data_dir / "test_data_START.out"
+    stop_p: Path = data_dir / "test_data_STOP.out"
+    data_start = import_data(start_p)
+    data_stop = import_data(stop_p)
+    data_stop, discarded = discard_rows_until_first_col_match(
+        data_ref=data_start,
+        data_to_process=data_stop,
+    )
+    assert discarded == 7729, f"Unexpected number of discarded points: {discarded}"
+    delays = calculate_delay(
+        data_start=data_start,
+        data_stop=data_stop,
+        frequency=500,
+    )
+    assert len(delays) == 32220, f"Unexpected number of points: {len(delays)}"
+    autodetection: tuple[NDArray[np.bool_], ...] = detect_signal(delays["femto"])
+    assert len(autodetection) == 1, f"Unexpected auto-detections: {len(autodetection)}"
+    signal_values: NDArray[np.int_] = delays["femto"][autodetection[0]]
+    range_center: float = (signal_values.max() + signal_values.min()) / 2
+    new_range_size: float = (signal_values.max() - signal_values.min()) * 8
+    min_delay = range_center - new_range_size * 2 / 5
+    max_delay = range_center + new_range_size * 3 / 5
+    assert np.isclose(min_delay, 58824679.9), f"Unexpected min_delay: {min_delay}"
+    assert np.isclose(max_delay, 60729103.9), f"Unexpected max_delay: {max_delay}"
+    mask = (min_delay <= delays["femto"]) & (delays["femto"] <= max_delay)
+    selected_delays = delays[mask]
+    assert len(selected_delays) == 2419, f"Unexpected delay sel.: {len(selected_delays)}"
+    with patch("NPET_DP.epoch_processing.helper_processing.config") as mock_config:
+        mock_config.sigma = 2.2
+        sigma_filtered, sigma_iter = recursive_sigma_filter(selected_delays)
+        assert len(sigma_filtered) == 1567, f"Unexpected sigma filtered: {len(sigma_filtered)}"
+        assert sigma_iter == 18, f"Unexpected sigma iterations: {sigma_iter}"
+    mean = np.mean(sigma_filtered["femto"])
+    sc_mean, mean_iter = auto_scale_num(mean)
+    mean_unit = get_unit("fs", mean_iter)
+    assert np.isclose(sc_mean, 59.5264), f"Unexpected mean: {mean}"
+    assert mean_unit == "ns", f"Unexpected mean unit: {mean_unit}"
+    std = np.std(sigma_filtered["femto"])
+    sc_std, std_iter = auto_scale_num(std)
+    std_unit = get_unit("fs", std_iter)
+    assert np.isclose(sc_std, 17.1517), f"Unexpected std: {sc_std}"
+    assert std_unit == "ps", f"Unexpected std unit: {std_unit}"
+    ret_rate: float = 100 * len(sigma_filtered) / len(delays)
+    assert np.isclose(ret_rate, 4.86, atol=0.005), f"Unexpected return rate: {ret_rate}"
