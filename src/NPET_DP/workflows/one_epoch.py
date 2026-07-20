c299a680ef6dbc32788fa730231b485e9edb8c59
MattStav <matej.stavinoha@volny.cz>
2026-07-20 15:52:37 +0200

    Updated one epoch workflow to use the NPETData structure

--- src/NPET_DP/workflows/one_epoch.py
+++ src/NPET_DP/workflows/one_epoch.py
@@ -1,19 +1,17 @@
 from pathlib import Path
 
 import numpy as np
-import numpy.typing as npt
 import typer
 from matplotlib import pyplot as plt
 
-from NPET_DP.processing.helpers import import_data, validate_inputs
+from NPET_DP.processing.data_struct import NPETData
 from NPET_DP.framework.config import config
 from NPET_DP.framework.constants import FEMTO
 from NPET_DP.framework.file_selection import user_file_select
 from NPET_DP.framework.path_handler import get_plot_path
 
 
-@validate_inputs
-def __plot_singular_data(data: npt.NDArray, name: str) -> None:
+def __plot_singular_data(data: NPETData, name: str) -> None:
     """
     Plot a time epoch of each measurement within a set bin of width 1/freq
     Showing the delay within each measurement time window.
@@ -24,7 +22,7 @@ def __plot_singular_data(data: npt.NDArray, name: str) -> None:
     typer.echo(f"Plotting measured data from {name}")
     # This "wraps" each timestamp into a time window of width 1/freq (one measurement period)
     # This shows where within each frequency cycle the measurement occurred
-    plot_data = np.mod(data["femto"], FEMTO / config.frequency)
+    plot_data = np.mod(data.femto, FEMTO / config.frequency)
     plt.scatter([*range(len(plot_data))], plot_data, s=0.01)
     plt.title("Modulo: Epoch / Measurement window")
     plt.savefig(get_plot_path(f"single_{name}"))
@@ -38,4 +36,4 @@ def main_one_epoch() -> None:
     except FileNotFoundError:
         return
     typer.echo(f"Importing data from {epoch_file_path}")
-    __plot_singular_data(import_data(epoch_file_path), epoch_file_path.stem)
+    __plot_singular_data(NPETData.from_path(epoch_file_path), epoch_file_path.stem)
