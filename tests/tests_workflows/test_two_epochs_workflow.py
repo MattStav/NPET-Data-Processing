34a072d0e95f513888ffe9fed81475fda3d8aa10
MattStav <matej.stavinoha@volny.cz>
2026-08-23 14:41:29 +0200

    Adjusted workflow tests to the new workfow

--- tests/tests_workflows/test_two_epochs_workflow.py
+++ tests/tests_workflows/test_two_epochs_workflow.py
@@ -3,14 +3,22 @@ from pathlib import Path
 import numpy as np
 from numpy.typing import NDArray
 
+from NPET_DP.framework.config import config
 from NPET_DP.processing.calculations import (
+    calc_fwhm,
     calculate_delay,
     detect_signal,
     discard_rows_until_first_col_match,
     recursive_sigma_filter,
 )
 from NPET_DP.processing.data_struct import NPETData
-from NPET_DP.processing.helpers import auto_scale_num, get_unit, import_data
+from NPET_DP.processing.helpers import (
+    auto_scale_num,
+    get_signal_xrange,
+    get_unit,
+    import_data,
+)
+from NPET_DP.workflows.helpers import rough_auto_range
 
 
 def test_two_epochs(data_dir: Path) -> None:
@@ -36,21 +44,21 @@ def test_two_epochs(data_dir: Path) -> None:
     assert len(delays) == 32220, f"Unexpected number of points: {len(delays)}"
     autodetection: tuple[NDArray[np.bool_], ...] = detect_signal(delays["femto"])
     assert len(autodetection) == 1, f"Unexpected auto-detections: {len(autodetection)}"
-    signal_values: NDArray[np.int_] = delays["femto"][autodetection[0]]
-    range_center: float = (signal_values.max() + signal_values.min()) / 2
-    new_range_size: float = (signal_values.max() - signal_values.min()) * 8
-    min_delay = range_center - new_range_size * 2 / 5
-    max_delay = range_center + new_range_size * 3 / 5
-    assert np.isclose(min_delay, 58824679.9), f"Unexpected min_delay: {min_delay}"
-    assert np.isclose(max_delay, 60729103.9), f"Unexpected max_delay: {max_delay}"
+    signal = delays[autodetection[0]]
+    sig_width, sig_place = calc_fwhm(signal)
+    assert np.isclose(sig_width, 41626.09), f"Unexpected signal width: {sig_width}"
+    assert sig_place == 59524067, f"Unexpected signal place: {sig_place}"
+    min_delay, max_delay = get_signal_xrange(sig_place, sig_width * 3)
+    assert np.isclose(min_delay, 59336750), f"Unexpected min_delay: {min_delay}"
+    assert np.isclose(max_delay, 60086019), f"Unexpected max_delay: {max_delay}"
     mask = (min_delay <= delays["femto"]) & (delays["femto"] <= max_delay)
     selected_delays = delays[mask]
     delay_sel = len(selected_delays)
-    assert delay_sel == 2419, f"Unexpected delay sel.: {delay_sel}"
+    assert delay_sel == 2270, f"Unexpected delay sel.: {delay_sel}"
     sigma_filtered, sigma_iter = recursive_sigma_filter(selected_delays, sigma_mult=2.2)
     sigma_f_num = len(sigma_filtered)
     assert sigma_f_num == 1567, f"Unexpected sigma filtered: {sigma_f_num}"
-    assert sigma_iter == 18, f"Unexpected sigma iterations: {sigma_iter}"
+    assert sigma_iter == 17, f"Unexpected sigma iterations: {sigma_iter}"
     mean = np.mean(sigma_filtered["femto"])
     sc_mean, mean_iter = auto_scale_num(mean)
     mean_unit = get_unit("fs", mean_iter)
@@ -81,21 +89,22 @@ def test_two_epochs_npet_data(data_dir: Path) -> None:
     assert len(delays) == 32220, f"Unexpected number of points: {len(delays)}"
     autodetection: tuple[NDArray[np.bool_], ...] = delays.detect_signal()
     assert len(autodetection) == 1, f"Unexpected auto-detections: {len(autodetection)}"
-    signal_range = delays.define_signal_range(autodetection[0])
-    assert np.isclose(signal_range[0], 58824679.9), (
-        f"Unexpected min_delay: {signal_range[0]}"
+    rough_auto_range(delays, autodetection[0])
+    assert np.isclose(config.min_delay, 59336750), (
+        f"Unexpected min_delay: {config.min_delay}"
     )
-    assert np.isclose(signal_range[1], 60729103.9), (
-        f"Unexpected max_delay: {signal_range[1]}"
-    )
-    mask = (signal_range[0] <= delays.femto) & (delays.femto <= signal_range[1])
+    assert np.isclose(
+        config.max_delay,
+        60086019,
+    ), f"Unexpected max_delay: {config.max_delay}"
+    mask = (config.min_delay <= delays.femto) & (delays.femto <= config.max_delay)
     selected_delays = delays.filter_range(mask)
     delay_sel = len(selected_delays)
-    assert delay_sel == 2419, f"Unexpected delay sel.: {delay_sel}"
+    assert delay_sel == 2270, f"Unexpected delay sel.: {delay_sel}"
     sigma_filtered, sigma_iter = selected_delays.recursive_sigma_filter(2.2)
     sigma_f_num = len(sigma_filtered)
     assert sigma_f_num == 1567, f"Unexpected sigma filtered: {sigma_f_num}"
-    assert sigma_iter == 18, f"Unexpected sigma iterations: {sigma_iter}"
+    assert sigma_iter == 17, f"Unexpected sigma iterations: {sigma_iter}"
     sc_mean, mean_unit = sigma_filtered.sc_mean
     assert np.isclose(sc_mean, 59.5264), f"Unexpected mean: {sc_mean}"
     assert mean_unit == "ns", f"Unexpected mean unit: {mean_unit}"
